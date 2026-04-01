8ce53d7984d6643523e54a119038273931af71ec
Rakibul Molla <rakibul.molla98@gmail.com>
2026-04-01 21:23:58 +0530

    navBar Added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { FiShoppingCart } from "react-icons/fi";
+const Navbar = ({ cartItems }) => {
+    return (
+        <div className="sticky top-0 z-50 bg-white">
+            <div className="shadow-sm py-2">
+                <div className="max-w-7xl mx-auto navbar ">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-5 w-5"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    {" "}
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M4 6h16M4 12h8m-8 6h16"
+                                    />{" "}
+                                </svg>
+                            </div>
+                            <ul
+                                tabIndex="-1"
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                            >
+                                <li>
+                                    <a href="#">Products</a>
+                                </li>
+                                <li>
+                                    <a href="#">Features</a>
+                                </li>
+                                <li>
+                                    <a href="#">Pricing</a>
+                                </li>
+                                <li>
+                                    <a href="#">Testimonials</a>
+                                </li>
+                                <li>
+                                    <a href="#">FAQ</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <h2 className="text-3xl bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent font-extrabold">DigiTools</h2>
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1 flex gap-8 font-semibold">
+                            <li>
+                                <a href="#">Products</a>
+                            </li>
+                            <li>
+                                <a href="#">Features</a>
+                            </li>
+                            <li>
+                                <a href="#">Pricing</a>
+                            </li>
+                            <li>
+                                <a href="#">Testimonials</a>
+                            </li>
+                            <li>
+                                <a href="#">FAQ</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="navbar-end gap-5">
+                        <div className="relative">
+                            <FiShoppingCart />
+
+                            {cartItems.length > 0 && <span className="badge badge-sm rounded-full p-1 w-4 h-4 badge-error absolute -top-2 -right-2">{cartItems.length}</span>}
+
+                        </div>
+                        <a
+                            href="#"
+                            className="relative group font-medium transition-colors duration-200 hover:text-purple-600"
+                        >
+                            Login
+                            <span className="absolute -bottom-0.5 left-0 w-0 h-0.5 bg-purple-600 transition-all duration-300 group-hover:w-full rounded-full" />
+                        </a>
+                        <a className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full text-white
+                        transition-all duration-300 ease-in-out
+                              hover:scale-105 hover:shadow-lg hover:shadow-purple-500/40">Get Started</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
